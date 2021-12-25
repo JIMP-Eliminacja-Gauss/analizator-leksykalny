e2059c98e670ba134da9dd46ae4b3b0c716a8a2a
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2021-12-25 16:54:23 +0100

    Do testowania

--- store.c
+++ store.c
@@ -1,13 +1,22 @@
 #include "store.h"
+#include <stdlib.h>
 
-void store_add_def(char *funame, int line_number, char *inpname) {
+store_t store_init() {
+    store_t new;
+    new.line_number[0] = -1; // -1 to indicate that nothing changed this
+    new.line_number[1] = -1; // value after the initialization
+    new.func_def = NULL;
+    new.func_call = NULL;
+}
+
+void store_add_proto(char *funame, int line_number, char *inpname, store_t *fstore) {
+   ; 
+}
+
+void store_add_def(char *funame, int line_number, char *inpname, store_t *fstore) {
     ;
 }
 
-void store_add_proto(char *funame, int line_number, char *inpname) {
-    ;
-}
-
-void store_add_call(char *funame, int line_number, char *inpname) {
+void store_add_call(char *funame, int line_number, char *inpname, store_t *fstore) {
     ;
 } 
--- store.h
+++ store.h
@@ -1,15 +1,19 @@
 #ifndef _STORE_H_IS_INCLUDED_
 #define _STORE_H_IS_INCLUDED_
 
-typedef struct {
+typedef struct elem {
     char *funame; // name of the function
-    int line_number;
+    int line_number[2];
     char *inpname; // FILE name
+    
+    struct elem *func_def;
+    struct elem *func_call; 
 } store_t;
 
-void store_add_def(char *funame, int line_number, char *inpname);
-void store_add_proto(char *funame, int line_number, char *inpname);
-void store_add_call(char *funame, int line_number, char *inpname);
+void store_add_def(char *funame, int line_number, char *inpname, store_t *fstore);
+void store_add_proto(char *funame, int line_number, char *inpname, store_t *fstore);
+void store_add_call(char *funame, int line_number, char *inpname, store_t *fstore);
+// store_add_call wywowalanie funkcji
 
 
 #endif
